fbdd59dc9eec5a3baad5366bf6fa48468a496033
sivaedara <40156294+sivaedara@users.noreply.github.com>
2020-07-29 21:39:39 -0700

    updated on july 29th

--- register-form/JS.js
+++ register-form/JS.js
@@ -1,11 +1,21 @@
 try{
-(function(){
+window.onload=function(){
+  (function(){
   document.getElementById('btnSubmit').onclick = function(){
-    
-    $('#tblDetails')
     console.log(document.getElementById('txtFstName').value);
-  };
+    var newrow = '<tr><td>' + $('#txtFstName').val() + '</td><td>' + $('#txtLstName').val() + '</td><td>'+ $('#ddlCity option:selected').text() +'</td><td><button id=edit >Edit</button></td></tr>';
+    $('#tblDetails').append(newrow);
+    
+   };
+  })();
+
+  if(document.getElementById('edit')!=null){
+     document.getElementById('edit').onclick = function(){
   
-})();
+     var row_index = $(this).parent().index();
+     row = $('#tblDetails').find('tr').eq(row_index);
+  };
+  }
+ }
 }catch(e){console.log(e);}
 
